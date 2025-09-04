f4ad271cd004a79c87c13171277795551eb522d1
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-09-05 01:03:15 +0530

    day1

---
+++ patterns.cpp
@@ -0,0 +1,192 @@
+
+//--------------------------------------------------------------------------Star Reactangle
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+// for(int i=1; i<=n ; i++)
+
+// {
+
+//     for(int j = 1 ; j<=n ;j++)
+//     {
+//         cout << " * ";
+//     }
+//     cout<<endl;
+// }
+
+// }
+//  *  *  *  *
+//  *  *  *  *
+//  *  *  *  *
+//  *  *  *  *
+
+
+
+
+// --------------------------------------------------------------------------Number Reactangle
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+// for(int i=1; i<=n ; i++)
+
+// {
+
+//     for(int j = 1 ; j<=n ;j++)
+//     {
+//         cout <<j<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// }
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
+// ---------------------------------------------------------Alphabetical Reactangle
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+
+// char a = 'A';
+// for(int i=1; i<=n ; i++)
+
+// {
+
+//     for(int j = 1 ; j<=n ;j++)
+//     {
+//         cout <<a<<" ";
+//         a++;
+//     }
+//     cout<<endl;
+// }
+
+// }
+// A B C D
+// E F G H
+// I J K L
+// M N O P
+
+
+// -------------------------------------------------------------reverse Alpha Reactangle
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+
+// char a = 'P';
+// for(int i=1; i<=n ; i++)
+
+// {
+
+//     for(int j = 1 ; j<=n ;j++)
+//     {
+//         cout <<a<<" ";
+//         a--;
+//     }
+//     cout<<endl;
+// }
+
+// }P O N M
+// L K J I
+// H G F E
+// D C B A
+
+// -------------------------------------------------------------------------------- Right angled Triangle
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+// for(int i=1; i<=n ; i++)
+
+// {
+
+//     for(int j = 1 ; j<=i ;j++)
+//     {
+//         cout << " * ";
+//     }
+//     cout<<endl;
+// }
+
+// }
+
+
+
+//------------------------------------------------------------------- Reverse Right angled Triangle
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+// for(int i=1; i<=n ; i++)
+
+// {
+
+//     for(int j = n ; j>=i; j--)
+//     {
+//         cout <<" * ";
+//     }
+//     cout<<endl;
+// }
+
+// }
+
+
+
+
+
+// //------------------------------------------------------------------- Alphabetical Right angled Triangle
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+// int n;
+// cout <<"Enter number of rows : ";
+// cin>>n;
+// for(int i=1; i<=n ; i++)
+
+// {
+//     char a = 'A';
+//     for(int j = 1 ; j<=i; j++)
+//     {
+//         cout <<a<<" ";
+//         a++;
+//     }
+//     cout<<endl;
+// }
+
+// }
+
+
