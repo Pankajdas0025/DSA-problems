9a1e47e07c56c9220fdfb9e65ef44ca1eafab7d3
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-09-08 01:48:06 +0530

    add new

---
+++ maxandminarr.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+
+    cout<<"Enter size Of Array : ";
+    int n;
+    cin>>n;
+    int A[n];
+
+    cout<<"Enter "<<n<<" Elemants: ";
+    for(int i=0; i<n;i++)
+    {
+        cin>>A[i];
+    }
+
+
+
+    cout<<"Array Elemnts Are : "<<endl;
+     for(int i=0; i<n;i++)
+    {
+        cout<<A[i]<<endl;
+    }
+ int max=A[0]; int min=A[1];
+     for(int i=0; i<n;i++)
+    {
+       if(A[i]>max)
+       {
+        max=A[i];
+       }
+       else if(A[i]<min)
+       {
+        min=A[i];
+       }
+    }
+     cout<<"Maxium element is :"<<max<<" and Minimium element is "<<min;
+
+}
---
+++ nthmaxandminArr.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+
+    cout<<"Enter size Of Array : ";
+    int n;
+    cin>>n;
+    int A[n];
+
+    cout<<"Enter "<<n<<" Elemants: ";
+    for(int i=0; i<n;i++)
+    {
+        cin>>A[i];
+    }
+
+
+
+    cout<<"Array Elemnts Are : "<<endl;
+     for(int i=0; i<n;i++)
+    {
+        cout<<A[i]<<endl;
+    }
+
+for(int i=5; i>0;i--)
+{
+   for(int j=0; j<5-1;j++)
+
+   {
+
+    if(A[j]>A[j+1])
+    {
+        int temp = A[j];
+        A[j]=A[j+1];
+        A[j+1]=temp;
+    }
+   }
+
+}
+
+ cout<<endl<<"Sorted array Elemnts Are : "<<endl;
+     for(int i=0; i<n;i++)
+    {
+        cout<<A[i]<<endl;
+    }
+
+
+
+cout<<" Enter nth possition : ";
+   int nn;
+   cin>>nn;
+   cout<<" Maximun "<<nn<<"th element is "<<A[n-nn]<< " and minimum "<<nn<< " th element "<<A[nn-1];
+
+
+
+
+
+
+
+}
